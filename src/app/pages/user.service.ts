aac2ed68781e9a6257924bd4c47a27f33a4a0e19
Cindy Michelle <cindy@kodefox.com>
2019-11-13 13:08:53 +0700

    wip

--- src/app/pages/user.service.ts
+++ src/app/pages/user.service.ts
@@ -1,46 +1,39 @@
+import { Storage } from '@ionic/storage';
+import { Observable } from 'rxjs';
 import { UserViewModel } from './../model/user.model';
 import { AsyncStorageService } from './../native/async-storage.service';
 import { User } from 'src/app/model/user.model';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   email?: string;
-  loadedUser?: UserViewModel;
+  userObservable: Observable<UserViewModel[]> = [];
 
-  constructor(
-    private storage: AsyncStorageService,
-    private db: AngularFirestore,
-  ) {
-    this.initUser();
-  }
+  constructor(private storage: Storage, private db: AngularFirestore) {
+    this.storage.get('token').then((value) => {
+      this.email = value;
 
-  async initUser() {
-    this.email = await this.storage.get('token');
-
-    const collection = await this.db
-      .collection<User>('users', (ref) =>
+      const collection = this.db.collection<User>('users', (ref) =>
         ref.where('email', '==', this.email).limit(1),
-      )
-      .snapshotChanges()
-      .pipe();
+      );
 
-    await collection.subscribe((docs) => {
-      docs.map((doc) => {
-        const data = doc.payload.doc.data();
-        const id = doc.payload.doc.id;
-
-        console.log('DATAAAA : ', data);
-
-        this.loadedUser = { id, ...data };
-      });
+      this.userObservable = collection.snapshotChanges().pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          });
+        }),
+      );
     });
   }
-
   getUser() {
-    return this.loadedUser;
+    return this.userObservable;
   }
 }
